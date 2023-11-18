2c2eb52628fa53e227e8bf5c6709c8b44b9907be
abhidesh10 <abhijitdeshmukh10@gmail.com>
2023-11-18 21:25:37 +0530

    optimise code logic for valid-anagram

--- strings/valid-anagram.js
+++ strings/valid-anagram.js
@@ -36,6 +36,13 @@ var isAnagram = function (s, t) {
             tMap.set(t[i], 1)
         }
     }
+    // Above code optimisation
+    /*
+        for (let i = 0; i < s.length; i++) {
+            sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+            tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+        }
+    */
 
     for (const [key, value] of sMap) {
         if (tMap.get(key) !== value) {
